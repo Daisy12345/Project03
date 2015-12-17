8664661e3ce48da415ad784822d346b4a5f3dbf8
RANZA <daisyranikesavan@gmail.com>
2015-12-18 09:25:20 +1100

    After header images working

--- tuition/app/controllers/application_controller.rb
+++ tuition/app/controllers/application_controller.rb
@@ -3,11 +3,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   
-	   # before_action :authenticate_admin!
+  # before_action :authenticate_admin!
  
-  		 before_action :authenticate_user!
+  before_action :authenticate_user!
 
-	   	
   # before_action :authenticate_admin!
 	  # def authenticate_admin_or_user!
 	 	 # if user_signed_in?
@@ -17,5 +16,5 @@ class ApplicationController < ActionController::Base
 	 	# # if !admin_signed_in? and user_signed_in?
 	 	# # 	redirect_to root_path, alert: "You must be logged in as an admin or user."	
 	 	# # end
-	  # end
+  # end
 end
